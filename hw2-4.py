99906f351b0c8c57acbb2642851ab4d339ecea66
p22yxi <p22yxi@fairfieldprep.org>
2021-09-26 18:46:01 -0700

    1

---
+++ hw2-4.py
@@ -0,0 +1,22 @@
+# Author : Yongdong Xi (Sep 26 2021)
+
+solution1 = 3
+print(solution1)
+
+solution2 = 8
+print(solution2)
+
+solution3 = -27
+print(solution3)
+
+solution4 = 9
+print(solution4)
+
+a = 1 
+b = 3
+c = 4 
+solution5 = (-b + (b ** 2 - 4 * a * c) ** (0.5)) / (2 * a)
+print(solution5)
+
+solution6 = (-b - (b ** 2 - 4 * a * c) ** (0.5)) / (2 * a)
+print(solution6)
